9d04d1dee642d75c5f39e26b09ebb86270dee12a
Lucy <happlb@msoe.edu>
2020-04-05 17:14:37 -0500

    hamburger menu filter added to events page

--- site/Script.js
+++ site/Script.js
@@ -28,4 +28,17 @@ function openPage(pageName, elmnt) {
 
     // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = '#f7a800';
+    w3_close();
+}
+
+function w3_open() {
+    document.getElementById("main").style.marginLeft = "30vw";
+    document.getElementById("mySidebar").style.width = "30vw";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+}
+function w3_close() {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
 }
